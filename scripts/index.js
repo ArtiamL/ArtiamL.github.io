c5f324d43c579cbcbae670af19c9edfc525c1de1
Myzostal <lukenewell@rocketmail.com>
2015-12-07 21:29:10 +0000

    Added index anims file and began work on landing page content

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+/*
+        Author: Luke Newell (@Myzostal)
+        Created: 06/12/15
+
+        Updated By: Luke Newell (@Myzostal)
+        Updated: 06/12/15
+*/
+
+$(document).ready(function(){
+    $('.initMessage, .about').css({display: "flex"}).hide().fadeIn(1500);
+});
